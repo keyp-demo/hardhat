64d7a2584024a0c9748c308a9e0482564b053d9f
loveyoubaby5120 <kaipan.1314.666@gmail.com>
2022-03-21 06:23:57 +0800

    chore: replace url

--- test/index.ts
+++ test/index.ts
@@ -25,14 +25,13 @@ describe("GameItem", function () {
     await gameItem.deployed();
 
     const playerAddress = "0x222fC57a9D6b9F8eD0d1d89E8213A01F3F671c21";
+    const data = "https://game.example/item-id-8u5h2m.json";
 
-    const awardItemTx = await gameItem.awardItem(
-      playerAddress,
-      "https://game.example/item-id-8u5h2m.json"
-    );
+    const awardItemTx = await gameItem.awardItem(playerAddress, data);
 
     await awardItemTx.wait();
 
     expect(await gameItem.ownerOf(1)).to.equal(playerAddress);
+    expect(await gameItem.tokenURI(1)).to.equal(data);
   });
 });
