2278b4cccddacab7ed117e82c67546c80795dc73
loveyoubaby5120 <kaipan.1314.666@gmail.com>
2022-03-21 06:22:09 +0800

    feat: gameItem contract

--- test/index.ts
+++ test/index.ts
@@ -17,3 +17,22 @@ describe("Greeter", function () {
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
+
+describe("GameItem", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const GameItem = await ethers.getContractFactory("GameItem");
+    const gameItem = await GameItem.deploy();
+    await gameItem.deployed();
+
+    const playerAddress = "0x222fC57a9D6b9F8eD0d1d89E8213A01F3F671c21";
+
+    const awardItemTx = await gameItem.awardItem(
+      playerAddress,
+      "https://game.example/item-id-8u5h2m.json"
+    );
+
+    await awardItemTx.wait();
+
+    expect(await gameItem.ownerOf(1)).to.equal(playerAddress);
+  });
+});
